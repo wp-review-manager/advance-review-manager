0009f5d841aa158f2b5cd79dc35898420f1e0329
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2024-01-29 09:49:08 +0600

    Dynamic Table

--- src/admin/Components/Admin.vue
+++ src/admin/Components/Admin.vue
@@ -1,30 +1,84 @@
 <template>
-  <div>
-    <!-- <div class="relative bg-blueGray-100">
-      <dashboard-navbar />
-      <header-stats />
-      <div class="px-4 md:px-10 mx-auto w-full -m-24">
-        <Dashboard/>
-        <router-view />
-        <footer-admin />
-      </div>
-    </div> -->
-    Home
+  <div class="wpf-dashboard">
+    <Table @edit-item="editHandler" @delete-item="deleteHandler" :columns="columns" :items="data"></Table>
   </div>
 </template>
 <script>
-import DashboardNavbar from "./Navbars/DashboardNavbar.vue";
-import HeaderStats from "./Headers/HeaderStats.vue";
-import FooterAdmin from "./Footers/FooterAdmin.vue";
-
-import Dashboard from './Dashboard.vue';
+import Table from './Common/Table.vue';
 export default {
   name: "admin-layout",
   components: {
-    DashboardNavbar,
-    HeaderStats,
-    FooterAdmin,
-    Dashboard
+    Table
   },
+  data() {
+    return {
+      columns: [
+        {
+          label: "Name",
+          field: "name",
+          sortable: true
+        },
+        {
+          label: "Category",
+          field: "category",
+          sortable: true
+        },
+        {
+          label: "Price",
+          field: "price",
+          sortable: true
+        },
+        {
+          label: "Brand",
+          field: "brand",
+          sortable: true
+        },
+        {
+          label: "Actions",
+          field: "actions"
+        }
+      ],
+      data: [
+        {
+          name: "Apple MacBook Pro 17",
+          category: "Silver",
+          price: "$2999",
+          brand: "Apple",
+          actions: {
+            edit: true,
+            delete: true
+          }
+        },
+        {
+          name: "Microsoft Surface Pro",
+          category: "White",
+          price: "$1999",
+          brand: "Microsoft",
+          actions: {
+            edit: false,
+            delete: true
+          }
+        },
+        {
+          name: "Magic Mouse 2",
+          category: "Black",
+          price: "$99",
+          brand: "Apple",
+          actions: {
+            edit: false,
+            delete: true
+          }
+        }
+      ]
+    }
+  },
+  methods: {
+    editHandler(item) {
+      console.log(item);
+    },
+    deleteHandler(item) {
+      console.log(item);
+    }
+  }
 };
 </script>
