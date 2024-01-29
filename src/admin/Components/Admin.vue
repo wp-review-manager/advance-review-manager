0af83b96c12bedc56fc22eb14430415da1d1886d
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2024-01-29 16:06:26 +0600

    Add search bar for table

--- src/admin/Components/Admin.vue
+++ src/admin/Components/Admin.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="wpf-dashboard">
+    <div class="p-2 flex flex-row justify-between items-center bg-white border border-gray-200 rounded-lg mb-3 ">
+      <div class="object-cover w-2/4 gap-x-8 rounded-t-lg h-96 md:h-auto md:rounded-none md:rounded-s-lg">
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+          </div>
+          <input type="search" id="default-search"
+            class="block WPRM-search-input w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-yellow-700 focus:border-yellow-700  dark:text-white"
+            placeholder="Search Mockups, Logos..." required>
+          <button type="submit" class="text-white absolute end-1.5 bottom-1.5 h-8  rounded-lg px-4 py-2 bg-yellow-700"
+            style="line-height: 1.04rem;">Search</button>
+        </div>
+      </div>
+      <div class="w-2/4 float-right leading-normal">
+        <button type="button"
+          class="float-right focus:outline-none text-white bg-yellow-700 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-1  dark:focus:ring-yellow-900">Create
+          Your Form
+        </button>
+      </div>
+    </div>
     <Table @edit-item="editHandler" @delete-item="deleteHandler" :columns="columns" :items="data"></Table>
   </div>
 </template>
@@ -55,7 +79,7 @@ export default {
           price: "$1999",
           brand: "Microsoft",
           actions: {
-            edit: false,
+            edit: true,
             delete: true
           }
         },
@@ -65,7 +89,7 @@ export default {
           price: "$99",
           brand: "Apple",
           actions: {
-            edit: false,
+            edit: true,
             delete: true
           }
         }
